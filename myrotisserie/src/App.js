dd86adbb2fd3a52c8730157f0427d0db52e24dc3
Damsbot64 <damien64durand@gmail.com>
2022-11-05 18:03:21 +0100

    creating Header and Footer

---
+++ myrotisserie/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
