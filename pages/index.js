23afbfa9122e255dfebe9781b3ee48bb59efdeb2
Damsbot64 <damien64durand@gmail.com>
2022-11-21 23:11:17 +0100

    added meta data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Body from "../src/Components/Body";
 import Footer from "../src/Components/Footer";
 import Header from "../src/Components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Rotisserie Harana</title>
+        <meta
+          name="google-site-verification"
+          content="fOXnzzGM8BauGzG8631OhoU4A7uKD-3-EDSX7dIKNa8"
+        />
+        <meta
+          name="description"
+          content="Rotisserie qui propose des poulets fermiers à la rotissoire"
+        />
+      </Head>
       <Header />
       <Body />
       <Footer />
