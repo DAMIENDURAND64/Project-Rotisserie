5387bb70206828c04c39c06f7ed5bb8552d967db
Damsbot64 <damien64durand@gmail.com>
2023-05-29 01:42:46 +0200

    prices changed and correct some issues

--- src/Components/Produits-Body.jsx
+++ src/Components/Produits-Body.jsx
@@ -6,21 +6,6 @@ import PouletLabelRouge from "./produits/PouletLabelRouge";
 import PorcManex from "./produits/PorcManex";
 import Patates from "./produits/Patates";
 
-const variants = {
-  initial: {
-    y: 300,
-  },
-  animate: {
-    y: 0,
-
-    transition: {
-      type: "spring",
-      bounce: 0.5,
-      duration: 0.9,
-    },
-  },
-};
-
 function ProduitsBody() {
   return (
     <div>
@@ -28,39 +13,35 @@ function ProduitsBody() {
         <h1 className="text-white text-lg md:text-2xl font-bold h-14 w-full  justify-center items-center flex bg-black-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-black-600">
           NOS PRODUITS
         </h1>
-        <div className="flex flex-col justify-center items-center p-1 my-2  md:w-5/6 space-y-6">
+        <div className="flex flex-col justify-center items-center p-1 my-2 w-5/6 space-y-6 md:w-[620px] lg:w-[850px]">
           <motion.section
-            initial={"initial"}
-            whileInView={"animate"}
-            viewport={{ once: true, amount: 0.1 }}
-            variants={variants}
+            initial={{ opacity: 0, x: -250 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1, bounce: 0.5 }}
           >
             <PouletIparla />
           </motion.section>
-          <hr className="w-4/6 my-2"></hr>
+          <hr className=" w-full my-2"></hr>
           <motion.section
-            initial={"initial"}
-            whileInView={"animate"}
-            viewport={{ once: true, amount: 0.1 }}
-            variants={variants}
+            initial={{ opacity: 0, x: 250 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1, bounce: 0.5 }}
           >
             <PouletLabelRouge />
           </motion.section>
-          <hr className="w-4/6 my-2 "></hr>
+          <hr className="w-full my-2 "></hr>
           <motion.section
-            initial={"initial"}
-            whileInView={"animate"}
-            viewport={{ once: true, amount: 0.1 }}
-            variants={variants}
+            initial={{ opacity: 0, x: -250 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1, bounce: 0.5 }}
           >
             <PorcManex />
           </motion.section>
-          <hr className="w-4/6 my-2"></hr>
+          <hr className="w-full my-2"></hr>
           <motion.section
-            initial={"initial"}
-            whileInView={"animate"}
-            viewport={{ once: true, amount: 0.1 }}
-            variants={variants}
+            initial={{ opacity: 0, x: 250 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ type: "spring", duration: 1, bounce: 0.5 }}
           >
             <Patates />
           </motion.section>
--- src/Components/produits/PorcManex.jsx
+++ src/Components/produits/PorcManex.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function PorcManex() {
   return (
     <div>
-      <div className="flex flex-col items-center w-full my-2 md:flex-row md:gap-4 justify-center">
+      <div className="flex flex-col items-center w-full my-2 md:flex-row md:gap-4 justify-center px-3">
         <div className="w-48 h-28 relative overflow-hidden object-fill rounded-md mb-2 md:min-w-[288px] md:min-h-[176px] ">
-          <Image
-            fill
-            alt="le porc manex qui nous vient du pays basque pour nous fournir des jarrets et echines de porc de qualité"
-            src="/Images/Logo-Manex.png"
-          />
+          <Link href={"https://porc-manex.com/"} target="_blank">
+            <Image
+              fill
+              alt="le porc manex qui nous vient du pays basque pour nous fournir des jarrets et echines de porc de qualité"
+              src="/Images/Logo-Manex.png"
+            />
+          </Link>
         </div>
-        <p className="text-base text-white md:text-lg text-center">
-          Le porc Manex provient de la ferme Belarrea située à St Etienne de
-          Baigorri.
-          <br /> Je vous propose a la rôtissoire l&apos;Echine ou le Jarret.
+        <p className="text-base text-white md:text-lg ">
+          Le porc Manex provient de la ferme Belarrea située à St Étienne de
+          Baigorri. Je vous propose à la rôtissoire l&apos;échine ou le jarret.
         </p>
       </div>
     </div>
