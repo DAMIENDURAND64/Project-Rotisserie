3f9f9af47e754a73009d4f8eba6e4cff729608f4
Damsbot64 <damien64durand@gmail.com>
2023-05-29 13:46:18 +0200

    delete border

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
   return (
     <div className=" bg-header h-8 bg-cover md:h-10 w-full">
-      <div className="flex justify-center items-end w-full border border-black  p-1">
+      <div className="flex justify-center items-end w-full h-full p-1">
         <p className="font-semibold text-sm">
           Made with &#x2661; by{" "}
           <Link
